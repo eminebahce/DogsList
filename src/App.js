ca80bd93e91f3fa5b4206c1e3020afe792b40f08
EmineBC <eminebahce@gmail.com>
2019-01-23 13:38:54 +0100

    react router finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import DogsListContainer from '../src/components/DogsListContainer';
+import {Route} from 'react-router-dom';
+import DogBreedImages from '../src/components/DogBreedImages';
+import DogBreedImagesContainer from '../src/components/DogBreedImagesContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <DogsListContainer />
+      <Route exact path="/" component={DogsListContainer} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImages} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
       </div>
     );
   }
